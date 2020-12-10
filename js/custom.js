59f6d4e523e7caee7c3e0fe6723cd3c64fef5c59
hyejin <dzzd777@naver.com>
2020-12-10 13:48:17 +0900

    Revert javascript

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+  let fitHeight = function(){
+    let imgHeight = $(".aboutImg").outerHeight();
+    // console.log(imgHeight);
+    $(".aboutTxt").height(imgHeight);
+
+    $(window).resize(function(){
+      let imgHeight = $(".aboutImg").outerHeight();
+    $(".aboutTxt").height(imgHeight);
+    })
+  }
+  fitHeight();
+
   let showNav = function() {
     $(".mobileIcon").click(function(){
       $("header").addClass("open");
